0335ea92ada7e61bb90f2a2aed14a272812a61a4
Spoorthipandhiri <spoorthipandhiri@gmail.com>
2024-08-12 18:26:07 +0530

    modified them

--- src/JenkCheck.java
+++ src/JenkCheck.java
@@ -2,5 +2,8 @@ class JenkCheck{
 public static void main(String args[])
 {
 System.out.println("HELLOWW PPL");
+Customer C =new Customer(101,"spoorthi","hyderabad","spoorthi@email.com");
+System.out.println(C.getLocation());
+System.out.println(C.getEmail());
 }
 }
